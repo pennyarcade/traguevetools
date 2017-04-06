e5bb10bbfe3c1c0192c79ed4351bd7c0d34c82d9
mtoennishoff <martin.toennishoff@scheideanstalt.de>
2017-04-06 11:04:31 +0200

    add libraries

--- bottle_app.py
+++ bottle_app.py
@@ -1,6 +1,9 @@
 
-# A very simple Bottle Hello World app for you to get started with...
 from bottle import default_app, route
+import requests
+import traceback
+import pprint
+
 
 @route('/')
 def hello_world():
@@ -9,7 +12,6 @@ def hello_world():
 @route('/csv/jitaores')
 def hello_world():
     output = ''
-
     output += 'csv / jita ores'
 
     return output
